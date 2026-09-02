6cc2e4a9560a00d5e3efe58e32bf277bff0d6f44
shukrypeter79-max <294992624+shukrypeter79-max@users.noreply.github.com>
2026-09-03 01:13:50 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import productsRoutes from "./api/products/products.routes";
 
 app.use("/api/products", productsRoutes);
+import countriesRoutes from "./api/countries/countries.routes";
+
+app.use("/api/countries", countriesRoutes);
